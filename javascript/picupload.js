64dc8892630ea01446f182ab47b1835bc5b3098f
Jose Mateo <jmateo6@outlook.com>
2013-03-21 13:40:25 -0400

    picupload.js takes picture and submits to php through ajax

--- javascript/picupload.js
+++ javascript/picupload.js
@@ -1,13 +1,21 @@
 $('document').ready(function(){
-	$('#getUserImage').change(function(){
-			var inputFile = document.getElementById('getUserImage').files[0];
+				var uploadPicture = $('#getUserImage').upload({
+				name: 'temp',
+				enctype: 'multipart/form-data',
+				action: 'upload.php',
+				onComplete: function(){},
+				onSelect: function(){
+			var inputFile = $(':file[name="temp"]')[0].files[0];
 			var renderedImage = new MegaPixImage(inputFile);
 			var printUserPicture = document.getElementById('printUserPicture');
 			renderedImage.render(printUserPicture, { maxWidth: 400, maxHeight: 400 });
 			var path = $('#getUserImage').val();
-			$('#path').html(path)
-			$.ajax({
-					
-			});
+			$('#path').html(path);
+			uploadPicture.submit();
+			}
+				});
+				
+	$('#getUserImage').change(function(){
+
 	});
 });
