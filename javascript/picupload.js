e20c674adfc6b80724621c8e9596b666dde49d9c
Jose Mateo <jmateo6@outlook.com>
2013-03-21 11:52:29 -0400

    picupload.js takes the input image and sends it to the php via ajax

---
+++ javascript/picupload.js
@@ -0,0 +1,8 @@
+$('document').ready(function(){
+	$('#getUserImage').change(function(){
+			var inputFile = document.getElementById('getUserImage').files[0];
+			var renderedImage = new MegaPixImage(inputFile);
+			var printUserPicture = document.getElementById('printUserPicture');
+			renderedImage.render(printUserPicture, { maxWidth: 400, maxHeight: 400 });
+	});
+});
