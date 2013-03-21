c70904449c5a49895a350ff28bf6bae7127c801f
Jose Mateo <jmateo6@outlook.com>
2013-03-21 12:32:10 -0400

    javascript

--- javascript/picupload.js
+++ javascript/picupload.js
@@ -4,5 +4,10 @@ $('document').ready(function(){
 			var renderedImage = new MegaPixImage(inputFile);
 			var printUserPicture = document.getElementById('printUserPicture');
 			renderedImage.render(printUserPicture, { maxWidth: 400, maxHeight: 400 });
+			var path = $('#getUserImage').val();
+			$('#path').html(path)
+			$.ajax({
+					
+			});
 	});
 });
