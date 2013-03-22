5b662e4df729170cac770639dadd7ecd5630888f
Jose Mateo <jmateo6@outlook.com>
2013-03-22 10:38:34 -0400

    latest

--- javascript/picupload.js
+++ javascript/picupload.js
@@ -11,7 +11,7 @@ $('document').ready(function(){
 		onSelect: function(){
 			var inputFile = $(':file[name="temp"]')[0].files[0];
 			var renderedImage = new MegaPixImage(inputFile);
-			var printUserPicture = document.getElementById('printUserPicture');
+			var printUserPicture = document.getElementsByClassName('printUserPicture');
 			renderedImage.render(printUserPicture, { maxWidth: 400, maxHeight: 400});
 			var path = $('#getUserImage').val();
 			$('#path').html(path);
